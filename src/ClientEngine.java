602b76e895a1d9f4933f1f0e8461b9a8f08370ac
Vlad <vlasmirnov@gmail.com>
2015-03-23 13:48:11 -0400

    working on engines

--- src/ClientEngine.java
+++ src/ClientEngine.java
@@ -5,8 +5,17 @@ public class ClientEngine extends Engine{
 	private void performAction(Message message){
 		System.out.println("client processing message " + message.message);
 		
-		switch(message.message){
+		String[] tokens = message.message.split("$");
 		
+		switch(tokens[0]){
+		
+		case "start server":
+			ServerEngine.startServer();
+			break;
+		case "connect to server":
+			break;
+		case "player update":
+			break;
 		default: 
 			System.out.println("What the hell even is this");
 		
--- src/ServerEngine.java
+++ src/ServerEngine.java
@@ -4,14 +4,18 @@ public class ServerEngine extends Engine{
 	static ServerEngine serverEngine;
 	
 	public static void startServer(){
-		serverEngine = new ServerEngine();
-		serverEngine.start();
+		if(serverEngine == null){
+			serverEngine = new ServerEngine();
+			serverEngine.start();
+		}		
 	}
 	
 	private void performAction(Message message){
 		System.out.println("server processing message " + message.message);
 		
-		switch(message.message){
+		String[] tokens = message.message.split("$");
+		
+		switch(tokens[0]){
 		
 		default: 
 			System.out.println("What the hell even is this");	
