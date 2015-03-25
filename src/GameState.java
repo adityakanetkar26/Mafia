3c4f77a03fcb440427709dee6c77d6767758d680
adityakanetkar26 <aditya.ykanetkar@gmail.com>
2015-03-25 11:53:39 -0400

    Mechanism for setting up player identities done

--- src/GameState.java
+++ src/GameState.java
@@ -1,17 +1,43 @@
 import java.util.HashMap;
+import java.util.Random;
+import java.lang.Math;
 
 
 public class GameState {
 
-	HashMap<String, Player> players = new HashMap<String, Player>();
+	HashMap<int, Player> players = new HashMap<int, Player>();
+	int badPlayerCount;
+	int goodPlayerCount;
+	
 	Player self;
 	
 	public void addPlayer(Player player){
 		players.put(player.id, player);
 	}
 	
-	public void assignPlayer(Player player){
+	public void assignIdentities(){
+		int totalSize = players.size();
+		badPlayerCount = int(Math.sqrt(double(totalSize)));
+		goodPlayerCount = totalSize - badPlayerCount;
+
+		Random randomGenerator = new Random();
 		
+		int remainingAssign = badPlayerCount;
+		while(remainingAssign != 0){
+			int randomInt = randomGenerator.nextInt(badPlayerCount);
+			Player p = players.get(randomInt);
+			boolean playerType = p.isGoodOrBad();
+
+			if(playerType == true){
+	
+				assignPlayer(p);
+				remainingAssign--;
+			}
+		}
+		
+	}
+	public void assignPlayer(Player player){
+		player.setBad();
 	}
 	
 	public void playerUpdateClient(String id, String update){
--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,38 @@
 
 public class Player {
 	
-	String id;
-	
-	public Player(String identifier){
+	int id;
+	boolean goodBad; //Good Player: True, Bad Player: False
+	boolean aliveDead; //Alive Player: True, Dead Player: False
+	String name;
+	ArrayList<Player> visiblePlayers;
+		
+	public Player(int identifier, String Name){
 		id = identifier;
+		name = Name;
+		goodBad = true;		//Assigning a player as good by default
 	}
 
+	public String Vote(){
+		//Create GUI to ask the player to vote against another player. 
+		//Get that vote and send to the server
+		String vote; 
+		return vote;
+	}
+	
+	public boolean isAliveOrDead(){
+		return aliveDead;
+	}
+
+	public void updateRole(boolean newState){
+		aliveDead = newState;
+	}
+	
+	public boolean isGoodOrBad(){
+		return goodBad;
+	}
+		
+	public void setBad(){
+		goodBad = false;
+	}
 }
