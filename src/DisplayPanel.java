c0a81b0862d70a269d214a31075b915746c82579
Vlad <vlasmirnov@gmail.com>
2015-03-25 22:28:05 -0400

    working on gui

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -16,8 +17,10 @@ import java.util.concurrent.LinkedBlockingQueue;
 import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -43,7 +46,7 @@ public class DisplayPanel extends JPanel{
 		
 		connectPanel = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		connectPanel.setBackground(new Color(100,100,100));
+		connectPanel.setBackground(new Color(0,20,40));
 		//connectPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
 		//connectPanel.setPreferredSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
 		//connectPanel.setPreferredSize(this.getSize());
@@ -53,6 +56,18 @@ public class DisplayPanel extends JPanel{
 		JButton connectButtonA = new JButton("Connect");
 		JTextField serverField = new JTextField("...or enter IP adress here.");
 		JButton connectButtonB = new JButton("Connect");
+		connectButtonA.setForeground(new Color(250,125,0));
+		connectButtonB.setForeground(new Color(250,125,0));
+		serverField.setForeground(new Color(250,125,0));
+		serverList.setForeground(new Color(250,125,0));
+		connectButtonA.setBackground(new Color(0,40,80));
+		connectButtonB.setBackground(new Color(0,40,80));
+		serverField.setBackground(new Color(0,40,80));
+		serverList.setBackground(new Color(0,40,80));
+		connectButtonA.setFont(new Font("Cooper Black", Font.PLAIN, 14));
+		connectButtonB.setFont(new Font("Cooper Black", Font.PLAIN, 14));
+		serverField.setFont(new Font("Cooper Black", Font.PLAIN, 14));
+		serverList.setFont(new Font("Cooper Black", Font.PLAIN, 14));
 
 		connectButtonA.addActionListener(new ActionListener(){
 
@@ -82,22 +97,28 @@ public class DisplayPanel extends JPanel{
 			}
 
 		});
-		JPanel connectTopPanel = new JPanel();
-		connectTopPanel.setBackground(new Color(50,50,50));
 		
+		ImageIcon icon = new ImageIcon(mafPics[0]);
+		JLabel connectTopLabel = new JLabel("Welcome to Mafia, son", icon, JLabel.CENTER);
+		connectTopLabel.setFont(new Font("Cooper Black", Font.PLAIN, 24));
+		connectTopLabel.setHorizontalTextPosition(JLabel.CENTER);
+		connectTopLabel.setVerticalTextPosition(JLabel.BOTTOM);
+		connectTopLabel.setBackground(new Color(50,50,50));
+		connectTopLabel.setForeground(new Color(250,125,0));
 		//connectTopPanel.setPreferredSize(new Dimension(Integer.MAX_VALUE, 200));
 		
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
+		c.gridx = 1;
 		c.gridy = 0;
-		c.gridwidth = 5;
+		c.gridwidth = 3;
 		c.weighty = 0.9;
-		connectPanel.add(connectTopPanel,c);
+		connectPanel.add(connectTopLabel,c);
 		
 		c.gridx = 1;
 		c.gridy = 1;
 		c.gridwidth = 1;
 		c.weighty = 0.1;
+		c.weightx = 0.1;
 		connectPanel.add(serverList, c);
 		
 		c.gridx = 3;
@@ -110,6 +131,19 @@ public class DisplayPanel extends JPanel{
 		c.gridx = 3;
 		connectPanel.add(connectButtonB, c);
 
+		c.gridx = 0;
+		c.gridy = 3;
+		c.weightx = 0.3;
+		connectPanel.add(new JLabel(), c);
+		
+		c.gridx = 2;
+		c.weightx = 0.1;
+		connectPanel.add(new JLabel(), c);
+		
+		c.gridx = 4;
+		c.weightx = 0.3;
+		connectPanel.add(new JLabel(), c);
+		
 		this.setLayout(new GridLayout(0,1));
 		this.add(connectPanel);
 		
@@ -141,15 +175,16 @@ public class DisplayPanel extends JPanel{
 	}//end repaint
 	
 	private void loadImages(){
-		mafPics = new BufferedImage[5];
-		civPics = new BufferedImage[5];
+		mafPics = new BufferedImage[2];
+		civPics = new BufferedImage[3];
+		
 		try {
-			unknown = ImageIO.read(new File("../img/unknown.png"));
+			//unknown = ImageIO.read(new File("/img/unknown.png"));
 			for(int a = 0; a < mafPics.length; a++){
-				mafPics[a] = ImageIO.read(new File("/img/m" + a + ".png"));
+				mafPics[a] = ImageIO.read(new File("img\\m" + a + ".png"));
 			}
 			for(int b = 0; b < civPics.length; b++){
-				civPics[b] = ImageIO.read(new File("/img/c" + b + ".png"));
+				civPics[b] = ImageIO.read(new File("img\\c" + b + ".png"));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
